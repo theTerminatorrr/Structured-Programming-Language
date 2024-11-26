31f58444cf35b41e540c440b7913a11992142917
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2024-11-27 00:20:26 +0600

    Capital and Small Letter Count in String.c

---
+++ Capital and Small Letter Count in String.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[1000];
+    char ch;
+    int i, capital, small, digit ;
+
+    printf("Enter a String: ");
+    gets(str);
+
+    i = capital = small = digit = 0;
+
+    while((ch = str[i]) != '\0')
+    {
+        if (str[i]>=65 && str[i]<=90 )
+        {
+            capital++;
+        }
+        else if (str[i]>=97 && str[i]<=120 )
+        {
+            small++ ;
+        }
+        else if (str[i]>=48 && str[i]<=97 )
+        {
+            digit++ ;
+        }
+        i++ ;
+    }
+    printf("The Number of Capital Letters: %d \n", capital);
+    printf("The Number of Small Letters: %d \n", small);
+    printf("The Number of Digits: %d \n", digit);
+
+    return 0;
+}
