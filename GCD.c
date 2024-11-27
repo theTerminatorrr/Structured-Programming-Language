89b347ec7cd13375abfac02671ca233805f6c4e7
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2024-11-27 13:09:38 +0600

    Create GCD.c

---
+++ GCD.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a, b, r;
+
+    printf("Enter the value of A: ");
+    scanf("%d",&a);
+
+    printf("Enter the value of b: ");
+    scanf("%d",&b);
+
+    while(b !=0)
+    {
+        r = a%b;
+        a = b;
+        b = r;
+    }
+    printf("GCD = %d\n",a);
+
+    return 0;
+}
+
